7a3c5ec4d78fee8d349c8550ea5b31545a6796cf
Rafael Nadalin <rafael.nadalin@outlook.com>
2023-10-22 11:21:11 -0300

    Update pdf_merger.py

--- app/pdf_merger.py
+++ app/pdf_merger.py
@@ -1,6 +1,19 @@
 import PyPDF2
 
 def merge_pdfs(file_list, output_name):
+    """
+    Merge a list of PDF files into a single PDF file.
+
+    Args:
+        file_list (list): List of file paths to the PDF files to be merged.
+        output_name (str): The name of the merged PDF file to be created.
+
+    Raises:
+        Exception: If there's an issue reading a PDF file.
+    
+    Prints:
+        Error messages for files that are not PDFs or cannot be merged.
+    """
     merge = PyPDF2.PdfMerger()
 
     for pdf_file in file_list:
